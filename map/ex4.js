ee4497be33ecae463f6208a2ba3e1aec31799d9a
Marc Bernini <bernini.marc@gmail.com>
2019-03-25 18:40:05 +0100

    ex4.js ok

--- map/ex4.js
+++ map/ex4.js
@@ -51,10 +51,16 @@ Tableau de sortie correspondant (toFixed transforme les nombres en chaînes)
 ]
 */
 
-function getCirclesProperties(radiuses) {
-}
-
-
+const getCirclesProperties = radiuses => {
+  const result = radiuses.map((resultFinal) => {
+    return {
+      radius: resultFinal,
+      circumference: (2 * Math.PI * resultFinal).toFixed(3), 
+      surface: (Math.PI * resultFinal * resultFinal).toFixed(3)  
+    };
+  });
+  return result;
+};
 
 // Ne pas modifier l'export
 module.exports = getCirclesProperties;
